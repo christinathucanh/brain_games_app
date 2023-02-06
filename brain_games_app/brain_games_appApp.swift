f7291a80f98013bfcf6a55167c68851a4a93fe62
Thuc Anh (Christina) Vu <95081865+christinathucanh@users.noreply.github.com>
2023-02-06 13:07:14 -0600

    Initial Commit

---
+++ brain_games_app/brain_games_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  brain_games_appApp.swift
+//  brain_games_app
+//
+//  Created by Thuc Anh "Christina" Vu on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct brain_games_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
